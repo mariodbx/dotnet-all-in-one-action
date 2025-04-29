8fddda9858dfc2bde0da37d547bd6ea880b9d4f2
mariodbx <mariodbx@users.noreply.github.com>
2025-04-29 09:43:07 +0000

    New Version: bump version to  Bump version to 44.0.0.22

--- src/workflows/runVersioning.ts
+++ src/workflows/runVersioning.ts
@@ -17,7 +17,7 @@ export async function runVersioning(): Promise<void> {
     // Wait and fetch the latest changes
     await Timer.wait(5000)
     core.info('Waiting for 5 seconds before ensuring the latest version...')
-    await gitManager.repo.pull('.', process.env['GITHUB_REF_NAME'])
+    await gitManager.repo.pull('.', 'oop' /*process.env['GITHUB_REF_NAME']*/)
     core.info('Fetched the latest changes from the repository.')
 
     // Get the latest commit message
