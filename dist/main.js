a2c0d2faee6ba1f3e9ba981382838a41a3ed4e7b
mariodbx <mariodbx@users.noreply.github.com>
2025-04-29 14:05:14 +0000

    hehe [no ci]

--- dist/main.js
+++ dist/main.js
@@ -20,40 +20,39 @@ export async function run() {
         await runHuskySetup();
     }
     // const git = new GitManager()
-    if (inputs.runTests) {
-        console.log('Running tests...');
-        await runTests();
-    }
-    if (inputs.runFormat) {
-        console.log('Running code formatting...');
-        await runFormat();
-    }
-    if (inputs.runVersioning) {
-        console.log('Running versioning...');
-        await runVersioning();
-    }
-    else {
-        return;
-    }
-    if (inputs.runDockerBuild) {
-        console.log('Running Docker build...');
-        await runDockerBuild();
-    }
-    if (inputs.runDockerPush) {
-        console.log('Running Docker push...');
-        await runDockerPush();
-    }
-    if (inputs.runPublish) {
-        console.log('Running publish...');
-        await runPublish();
-    }
-    if (inputs.runRelease) {
-        console.log('Running release...');
-        await runRelease();
-    }
-    if (inputs.runMigrations) {
-        console.log('Running migrations...');
-        await runMigrations();
-    }
+    // if (inputs.runTests) {
+    console.log('Running tests...');
+    await runTests();
+    // }
+    // if (inputs.runFormat) {
+    console.log('Running code formatting...');
+    await runFormat();
+    // }
+    // if (inputs.runVersioning) {
+    console.log('Running versioning...');
+    await runVersioning();
+    // } else {
+    //   return
+    // }
+    // if (inputs.runDockerBuild) {
+    console.log('Running Docker build...');
+    await runDockerBuild();
+    // }
+    // if (inputs.runDockerPush) {
+    console.log('Running Docker push...');
+    await runDockerPush();
+    // }
+    // if (inputs.runPublish) {
+    console.log('Running publish...');
+    await runPublish();
+    // }
+    // if (inputs.runRelease) {
+    console.log('Running release...');
+    await runRelease();
+    // }
+    // if (inputs.runMigrations) {
+    console.log('Running migrations...');
+    await runMigrations();
+    // }
     console.log('Action completed successfully.');
 }
